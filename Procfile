7059fdbcacfee1cc666ea806e846c2af4ea3576d
Devam Sisodraker <devam@sisodraker.com>
2020-05-30 15:14:56 -0700

    3:14 pm saturday

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: FLASK_APP=main.py FLASK_RUN_PORT=$PORT flask run
+web: FLASK_APP=main.py FLASK_RUN_PORT=$PORT FLASK_RUN_HOST=0.0.0.0 flask run
--- src/components/DonatePage.js
+++ src/components/DonatePage.js
@@ -68,11 +68,10 @@ function DonatePage() {
                     </Form.Row>
                     <p></p>
             
-                <Form.Row>
-                    <Form.Group as={Col} md="6" controlId="validationCustom03" style= {{marginBottom: 30}} >
-                    <Form.Label>Other Amount</Form.Label>
-                    <Form.Control style={{marginBottom: 50}} type="text" placeholder="CAD $"  />
-                    
+                    <Form.Row>
+                        <Form.Group as={Col} md="6" controlId="validationCustom03" style= {{marginBottom: 30}} >
+                        <Form.Label>Other Amount</Form.Label>
+                        <Form.Control style={{marginBottom: 50}} type="text" placeholder="CAD $"  />
                     </Form.Group>
                     
                 
